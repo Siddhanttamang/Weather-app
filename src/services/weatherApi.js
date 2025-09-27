2d9ef057c5897be6d37ac7245e74784fb2a2bad2
Siddhanttamang <tsiddhant2003@gmail.com>
2025-09-28 00:26:22 +0545

    error-fixed

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,17 +1,9 @@
-const API_KEY = import.meta.env.API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-const BASE_URL="http://api.openweathermap.org/data/2.5"
+const BASE_URL = "http://api.openweathermap.org/data/2.5";
 
-export const getWeatherData= async(city)=>{
+export const getWeatherData = async (city) => {
     const response = await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data= await response.json();
+    const data = await response.json();
     return data;
-
-}
-
-// const WeatherData= await getWeatherData("birtamod");
-// console.log("Weather: ",WeatherData);
-//  if(WeatherData){
-//     console.log("Temperature: ",WeatherData.main.temp);
-//     console.log("Feels like: ",WeatherData.main.feels_like);
-//  }
+};
