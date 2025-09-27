9424fc5fe98098bd40da649ad01ecae4d9eb411c
Siddhanttamang <tsiddhant2003@gmail.com>
2025-09-28 00:36:30 +0545

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './css/index.css'
 import Home from './pages/Home'
 import { WeatherProvider } from './contexts/WeatherContext'
